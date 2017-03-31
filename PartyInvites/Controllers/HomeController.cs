0613bd651837d88923b98f70307e49f7a3374d2d
Anthony Leif <tonyleif@hotmail.com>
2017-03-31 17:02:33 -0500

    Setting the start URL

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -8,11 +8,14 @@ namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View();
+        }
+        public ViewResult RsvpForm()
+        {
             return View();
         }
     }
