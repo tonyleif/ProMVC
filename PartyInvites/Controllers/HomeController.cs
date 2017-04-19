f225d8226574ee009ee1cd88a8a9231ce9db6aa8
aleif <aleif@BLML-GJMRN32>
2017-04-18 21:26:49 -0500

    Moving to VS2017

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -14,9 +15,18 @@ namespace PartyInvites.Controllers
             ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
             return View();
         }
+
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: Email response to the party organizer
+            return View("Thanks", guestResponse);
         }
     }
 }
